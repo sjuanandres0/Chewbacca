b4b38ce1f415a84401e09e29afbb90d776de39f4
Juan Simonetti <sjuanandres0@gmail.com>
2022-01-23 20:07:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 import requests
 
-test_secret = os.environ.get('TEST_SECRET')# os.environ['test_secret'] 
+test_secret = os.environ.get('test_secret')# os.environ['test_secret'] 
 print('test_secret is: ', test_secret)
-bot_id = os.environ.get('BOT_ID')# os.environ['bot_id']
-chat_id = os.environ.get('CHAT_ID')# os.environ['chat_id']
+bot_id = os.environ.get('bot_id')# os.environ['bot_id']
+chat_id = os.environ.get('chat_id')# os.environ['chat_id']
 message = 'test_message'
 api_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_id, chat_id, message)
 requests.get(api_url)
