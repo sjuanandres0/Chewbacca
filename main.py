98ce47773812aa1d138fb50fb86b80c4e67dcc76
Juan Simonetti <sjuanandres0@gmail.com>
2022-01-23 19:57:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import requests
 
+test_scret = os.environ['test_secret'] 
+print('test_secret is: ', test_scret)
 bot_id = os.environ['bot_id']
 chat_id = os.environ['chat_id']
 message = 'test_message'
