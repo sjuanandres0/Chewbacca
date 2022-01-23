3f37e1e6b42eb10beb7b2e94ba51d9e963794ce8
Juan Simonetti <sjuanandres0@gmail.com>
2022-01-23 19:38:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ import requests
 #chat_id = 'chat_id_test'
 message = 'test_message'
 api_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_id, chat_id, message)
-print(api_url)
 requests.get(api_url)
