1e1ba3d474c5cb82f0e6b411ddd889a94db8f9bc
Juan Simonetti <sjuanandres0@gmail.com>
2022-01-23 20:33:12 +0100

    adding yfinance data

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
 import requests
+import yfinance as yf
 
-test_secret = os.environ.get('test_secret')# os.environ['test_secret'] 
-print('test_secret is: ', test_secret)
-bot_id = os.environ.get('bot_id')# os.environ['bot_id']
-chat_id = os.environ.get('chat_id')# os.environ['chat_id']
+bot_id = os.environ.get('bot_id')
+chat_id = os.environ.get('chat_id')
 message = 'test_message'
 api_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_id, chat_id, message)
 requests.get(api_url)
+
+
+ticker_list = ['BTC-USD','ETH-USD','MSFT','TSLA','GOOG','AAPL']
+for ticker_lookup in ticker_list:
+    ticker = yf.Ticker(ticker_lookup)
+    #print(ticker.info)
+    # get historical market data
+    hist = ticker.history(period="max")
+    #print(hist)
+    #hist.to_csv('hist_'+ticker_lookup+'.csv')
+    (hist['Close'].pct_change()*100).iloc[-1]
+    message = 'Daily Pct change for {0:,.2f}'.format(ticker_lookup)
+    api_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_id, chat_id, message)
+    requests.get(api_url)
