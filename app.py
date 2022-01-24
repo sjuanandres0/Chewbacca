b7bedc1c2e488973299e1ba67780d8443a30f762
Juan Simonetti <sjuanandres0@gmail.com>
2022-01-24 21:57:05 +0100

    heroku deploy

--- app.py
+++ app.py
@@ -1,29 +1,52 @@
 import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+
+import pandas as pd
+from datetime import datetime
+
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+df = pd.read_csv('ticker_data.csv', index_col='Date', parse_dates=True)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2("Hello Galaxy, I'm Chewbacca 🤓🚀📈"),
+    dcc.Checklist(
+        id='toggle-rangeslider',
+        options=[{'label': 'Include Rangeslider', 
+                  'value': 'slider'}],
+        value=['slider']
+    ),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+        options=[{'label': i, 'value': i} for i in ['BTC-USD', 'AAPL', 'MSFT']],
+        value='BTC-USD'
     ),
-    html.Div(id='display-value')
+    dcc.Graph(id="graph"),
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-                [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(
+    Output("graph", "figure"), 
+    [Input("toggle-rangeslider", "value")])
+def display_candlestick(value):
+    fig = go.Figure(go.Candlestick(
+        x=df['Date'],
+        open=df['AAPL.Open'],
+        high=df['AAPL.High'],
+        low=df['AAPL.Low'],
+        close=df['AAPL.Close']
+    ))
+
+    fig.update_layout(
+        xaxis_rangeslider_visible='slider' in value
+    )
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import os
 import requests
 import yfinance as yf
+import pandas as pd
 
 bot_id = os.environ.get('bot_id')
 chat_id = os.environ.get('chat_id')
-message = 'test_message'
+#message = 'test_message'
+message = 'Check: https://chewbacca22.herokuapp.com/'
 api_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_id, chat_id, message)
 requests.get(api_url)
 
-
+base = pd.DataFrame(columns=['Date','Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits', 'ticker'])
 ticker_list = ['BTC-USD','ETH-USD','MSFT','TSLA','GOOG','AAPL']
 for ticker_lookup in ticker_list:
     ticker = yf.Ticker(ticker_lookup)
     #print(ticker.info)
     # get historical market data
-    hist = ticker.history(period="max")
-    #print(hist)
-    #hist.to_csv('hist_'+ticker_lookup+'.csv')
+    hist = ticker.history(period="max").reset_index()
+    hist['ticker'] = ticker_lookup
+    base = base.append(hist, ignore_index=True)
     pct_change = (hist['Close'].pct_change()*100).iloc[-1]
     message = "{} Daily Pct change is {:,.2f}".format(ticker_lookup, pct_change)
-    api_url = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}'.format(bot_id, chat_id, message)
-    requests.get(api_url)
+
+base.to_csv('Chewbacca/ticker_data.csv', index=False)
