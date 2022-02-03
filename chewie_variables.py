8253a440cf8d6a391094ce73d86fbe44073403d5
Juan Simonetti <sjuanandres0@gmail.com>
2022-02-03 07:43:29 +0100

    More tickers

--- chewie_variables.py
+++ chewie_variables.py
@@ -1,7 +1,6 @@
 
 # List of tickers in scope
-ticker_list = ['^GSPC','BTC-USD','ETH-USD','MSFT','TSLA','GOOG','AAPL']
-
+ticker_list = ['BTC-USD','ETH-USD','^GSPC','MSFT','TSLA','GOOG','GOOGL','AAPL','FB','BYND','NVDA','JNJ','JPM','V','PG','XOM','MA','BAC','DIS','PFE','CVX','ADBE','KO','PEP','CSCO','ABT','CRM','ACN','NFLX','WMT','INTC','QCOM','MCD','NKE','AI','T','AMD','MDT','PM','HON','GS','BLK','SBUX','CAT','DE','BKNG','MMM','GM','F','FIS','FDX','MRNA','PFE']
 
 # Chewbacca Heroku Link
 chewie_url = 'https://chewbacca22.herokuapp.com/'
